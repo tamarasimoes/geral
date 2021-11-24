f3c6eea338a3f77b38da1637e4066139795c4ef0
Tamara Simões <tamara.simoes@betrybe.com>
2021-11-24 17:44:43 -0300

    Estudando B4 - Course

---
+++ js/exercicios-4-1.js
@@ -0,0 +1,37 @@
+const a = 5;
+const b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+const x = 6;
+const y = 7;
+
+if (x > y) {
+    console.log("O maior número é 6");
+}
+
+else {
+    console.log("O maior número é 7");
+}
+
+const value = 0;
+
+if (value > 0) {
+    console.log("Positive");
+}
+
+else if (value < 0) {
+    console.log("Negative");
+}
+
+else {
+    console.log("Zero");
+}
+
+const catetoOposto;
+const catetoAdjacente;
+const hipotenusa;
--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,89 @@ birthYear = "2030";
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = { //é um objeto
+    firstName: 'Tamara',
+    lastName: 'Simões',
+};
+const patientEmail = 'tamara@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = (base * 2) + (height * 2);
+console.log(perimeter);
+
+const note = 88;
+
+if (note >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+
+else if (note < 80 && note >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+
+else {
+    console.log("Você foi reprovada(o)")
+}
+
+// && - AND
+// || - OR
+// ! - NOT
+
+const currentHour = 1;
+let message;
+
+if (currentHour >=22) {
+    console.log("Não deveríamos comer nada, é hora de dormir");
+}
+
+else if (currentHour >=18 && currentHour < 22) {
+    console.log("Rango da noite, vamos jantar :D");
+}
+
+else if (currentHour >=14 && currentHour < 18) {
+    console.log("Vamos fazer um bolo para o café da tarde?");
+}
+
+else if (currentHour >=11 && currentHour < 11) {
+    console.log("Hora do almoço!");
+}
+
+else if (currentHour >=4 && currentHour < 11) {
+    console.log("Hmmm, cheiro de café recém passado");
+}
+else {
+    console.log("Dormindo");
+}
+
+let candidate = "lista";
+
+switch (candidate) {
+    case "aprovada":
+        console.log("Parabéns, você passou!");
+        break;
+
+    case "lista":
+        console.log("Você está em lista de espera");
+        break;
+
+    case "reprovada":
+        console.log("Você não passou :(");
+        break;
+
+    default:
+        console.log("Situação não identificada");
+}
