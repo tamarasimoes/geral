1677fa4ac88100e7a581ab347f1889ba615fa834
Tamara Simões <tamara.simoes@betrybe.com>
2021-11-26 15:27:02 -0300

    Exercicios Bloco 4.2

--- js/exercicios-4-1.js
+++ js/exercicios-4-1.js
@@ -1,37 +1,96 @@
-const a = 5;
-const b = 3;
+// const a = 5;
+// const b = 3;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
 
-const x = 6;
-const y = 7;
+// const x = 6;
+// const y = 7;
 
-if (x > y) {
-    console.log("O maior número é 6");
+// if (x > y) {
+//     console.log("O maior número é 6");
+// }
+
+// else {
+//     console.log("O maior número é 7");
+// }
+
+// const value = 0;
+
+// if (value > 0) {
+//     console.log("Positive");
+// }
+
+// else if (value < 0) {
+//     console.log("Negative");
+// }
+
+// else {
+//     console.log("Zero");
+// }
+
+// const catetoOposto = 90;
+// const catetoAdjacente = 10;
+// const hipotenusa = 50;
+
+// if (catetoOposto + catetoAdjacente + hipotenusa == 180) {
+//     console.log(true);
+// }
+
+// else {
+//     console.log(false);
+// }
+
+// const xadrez = "CAVALO";
+// const result = xadrez.toLowerCase();
+
+// switch (result) {
+//     case "rei":
+//         console.log("Todas as direções");
+//         break;
+    
+//     case "rainha": 
+//         console.log("Horizontal, vertical e diagonal, sem pular peças");
+//         break;
+
+//     case "bispo":
+//         console.log("Diagonal");
+//         break;
+
+//     case "cavalo":
+//         console.log("Vertical, horizontal, pula peças");
+//         break;
+
+//     case "torre":
+//         console.log("Vertical, horizontal, não pula peças");
+//         break;
+
+//     case "peão":
+//         console.log("Uma casa para frente e captura peças na diagonal");
+//         break;
+    
+//     default:
+//         console.log("Peça não identificada");
+// }
+
+const c = 3;
+const d = 7;
+const e = 2;
+
+
+//par ou ímpar, ainda fazendo
+let par = ((num1 + num2) % 2) === 0;
+console.log(par);
 }
 
-else {
-    console.log("O maior número é 7");
-}
+// else {
+//     console.log(false);
+// }
 
-const value = 0;
+// const note;
 
-if (value > 0) {
-    console.log("Positive");
-}
+// if ()
 
-else if (value < 0) {
-    console.log("Negative");
-}
-
-else {
-    console.log("Zero");
-}
-
-const catetoOposto;
-const catetoAdjacente;
-const hipotenusa;
--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,153 @@
-const myName = "Tamara";
-const birthCity = "Belo Horizonte";
-let birthYear = "1988";
-birthYear = "2030";
+// const myName = "Tamara";
+// const birthCity = "Belo Horizonte";
+// let birthYear = "1988";
+// birthYear = "2030";
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
 
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = { //é um objeto
-    firstName: 'Tamara',
-    lastName: 'Simões',
-};
-const patientEmail = 'tamara@email.com';
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = { //é um objeto
+//     firstName: 'Tamara',
+//     lastName: 'Simões',
+// };
+// const patientEmail = 'tamara@email.com';
 
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
-console.log(typeof patientEmail);
-console.log(typeof patientAge);
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge);
 
-const base = 5;
-let height = 8;
-const area = base * height;
+// const base = 5;
+// let height = 8;
+// const area = base * height;
 
-console.log(area);
+// console.log(area);
 
-const perimeter = (base * 2) + (height * 2);
-console.log(perimeter);
+// const perimeter = (base * 2) + (height * 2);
+// console.log(perimeter);
 
-const note = 88;
+// const note = 88;
 
-if (note >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!");
-}
+// if (note >= 80) {
+//     console.log("Parabéns, você foi aprovada(o)!");
+// }
 
-else if (note < 80 && note >= 60) {
-    console.log("Você está na nossa lista de espera");
+// else if (note < 80 && note >= 60) {
+//     console.log("Você está na nossa lista de espera");
+// }
+
+// else {
+//     console.log("Você foi reprovada(o)")
+// }
+
+// // && - AND
+// // || - OR
+// // ! - NOT
+
+// const currentHour = 1;
+// let message;
+
+// if (currentHour >=22) {
+//     console.log("Não deveríamos comer nada, é hora de dormir");
+// }
+
+// else if (currentHour >=18 && currentHour < 22) {
+//     console.log("Rango da noite, vamos jantar :D");
+// }
+
+// else if (currentHour >=14 && currentHour < 18) {
+//     console.log("Vamos fazer um bolo para o café da tarde?");
+// }
+
+// else if (currentHour >=11 && currentHour < 11) {
+//     console.log("Hora do almoço!");
+// }
+
+// else if (currentHour >=4 && currentHour < 11) {
+//     console.log("Hmmm, cheiro de café recém passado");
+// }
+// else {
+//     console.log("Dormindo");
+// }
+
+// let candidate = "lista";
+
+// switch (candidate) {
+//     case "aprovada":
+//         console.log("Parabéns, você passou!");
+//         break;
+
+//     case "lista":
+//         console.log("Você está em lista de espera");
+//         break;
+
+//     case "reprovada":
+//         console.log("Você não passou :(");
+//         break;
+
+//     default:
+//         console.log("Situação não identificada");
+// }
+
+// let num1 = 5;
+// let num2 = 13;
+// let num3 = 15;
+
+
+// if ((num1 % 2) || (num2 % 2) || (num3 % 2)) {
+//     console.log(true);
+// } 
+// else {
+//     console.log(false);
+// }
+
+const custoProduto = 17;
+const imposto = (custoProduto * 20) / 100;
+const valorVenda = 30;
+
+if (custoProduto < 0 || valorVenda < 0 || imposto < 0) {
+    console.log("Erro: um dos valores está zerado!");
 }
 
 else {
-    console.log("Você foi reprovada(o)")
+    const custoTotal = custoProduto + imposto;
+    const lucro = valorVenda - custoTotal;
+    console.log("O custo total do produto é de R$ " + custoTotal);
+    console.log("O lucro obtido na venda é de R$ " + lucro);
 }
 
-// && - AND
-// || - OR
-// ! - NOT
+//Calculadora salário líquido
 
-const currentHour = 1;
-let message;
+const sBruto = 2500.00;
+var sLiquido;
+var sTotal;
 
-if (currentHour >=22) {
-    console.log("Não deveríamos comer nada, é hora de dormir");
+if (sBruto <= 1556.94) {
+    const inss = (sBruto * 9) / 100;
+    sLiquido = sBruto - inss;
 }
 
-else if (currentHour >=18 && currentHour < 22) {
-    console.log("Rango da noite, vamos jantar :D");
+else if (sBruto >= 1903.99 && sBruto <= 2594.92) {
+    const inss = (sBruto * 9) / 100;
+    sLiquido = sBruto - inss;
 }
 
-else if (currentHour >=14 && currentHour < 18) {
-    console.log("Vamos fazer um bolo para o café da tarde?");
+else if (sBruto >= 2594.93 && sBruto <= 5189.82) {
+    const inss = (sBruto * 11) / 100;
+    sLiquido = sBruto - inss;
 }
 
-else if (currentHour >=11 && currentHour < 11) {
-    console.log("Hora do almoço!");
-}
-
-else if (currentHour >=4 && currentHour < 11) {
-    console.log("Hmmm, cheiro de café recém passado");
-}
 else {
-    console.log("Dormindo");
+    sLiquido = sBruto - 570.88;
 }
 
-let candidate = "lista";
-
-switch (candidate) {
-    case "aprovada":
-        console.log("Parabéns, você passou!");
-        break;
-
-    case "lista":
-        console.log("Você está em lista de espera");
-        break;
-
-    case "reprovada":
-        console.log("Você não passou :(");
-        break;
-
-    default:
-        console.log("Situação não identificada");
+if (sLiquido >= 1903.99 && sLiquido <= 2826.65) {
+    const irpf = (sLiquido * 7,5) / 100;
+    sTotal = sLiquido - irpf;
 }
+
+console.log("Salário líquido é de R$ " + sTotal);
