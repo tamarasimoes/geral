e0cdecfdb81807ee93ff38ab06533071c0ea64fb
Tamara Simões <tamara.simoes@betrybe.com>
2022-01-04 19:23:56 -0300

    Bloco 6.1 continuando

--- js/script-form.js
+++ js/script-form.js
@@ -1,6 +1,5 @@
-function createStateOptions() {
-    let states = document.getElementById('state');
-    let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+let states = document.getElementById('state');
+let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
   
     for (let index = 0; index < stateOptions.length; index += 1) {
       let option = document.createElement('option');
@@ -8,4 +7,3 @@ function createStateOptions() {
       option.value = stateOptions[index];
       states.appendChild(option);
     }
-  }
